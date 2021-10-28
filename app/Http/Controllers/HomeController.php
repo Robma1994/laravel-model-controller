e89a2a238f52a153e1b5dc38067888d0c3e96771
Roberto <robertomartinonwg@gmail.com>
2021-10-28 17:08:14 +0200

    recupero dati, trasmissione dati alla view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,7 @@ class HomeController extends Controller
 {
     public function home(){
         $movies = Movie::all();
+        @dump($movies);
         return view('home', compact('movies'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,10 @@
-<div class="container">
-    @foreach ($movies as $movie) {
-        <h1>{{ $movie['title'] }}</h1>
-        }   
-    @endforeach
-</div>
+@foreach ($movies as $movie) 
+    <div class="container">
+        <h1> {{ $movie['title'] }} </h1>
+        <h2> {{$movie['original_title']}}</h2>
+        <h3> {{$movie['nationality']}} </h3>
+        <h3>{{$movie['date']}}</h3>
+        <h3>{{$movie['vote']}}</h3>
+    </div>
+ 
+@endforeach
